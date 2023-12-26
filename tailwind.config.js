100f559355f696ccdee25becbe3aecd21c21269a
Shadow19981377 <sha1234dow@gmail.com>
2023-12-26 09:58:44 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.htm'],
+  theme: {
+    extend: {
+      transitionDuration: {
+        400: '400ms'
+      }
+    },
+  },
+  plugins: [],
+}
+
